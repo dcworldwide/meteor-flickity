7c39a0c323b1ae8675fabb15a1e5619f5bc49be7
James Hatfield <jhat.home@gmail.com>
2015-06-18 21:56:28 -0500

    updated git repo url to point to this repo

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'emenoh:flickity',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'GPL v3 Flickity Carousel Widget by ',
+  summary: 'GPL v3 Flickity Carousel Widget by Metafizzy',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/metafizzy/flickity',
+  git: 'https://github.com/emenoh/meteor-flickity',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
